70d134d4a2e68145ab55a2c33c47a8a299274bbb
ceyhunsahin <csahin17072010@gmail.com>
2020-12-18 23:21:46 +0100

    add dates

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid} from '@material-ui/core';
-
+import CountUp from 'react-countup';
 
 import styles from './Cards.module.css';
 
@@ -16,24 +16,45 @@ const Cards = ({data : {confirmed, recovered, deaths, lastUpdate}  }) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">{confirmed.value}</Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                            start = {0}
+                            end = {recovered.value}
+                            duration = {2.5}
+                            separator = ","
+                             />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography variant="h5">{recovered.value}</Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                            start = {0}
+                            end = {confirmed.value}
+                            duration = {2.5}
+                            separator = ","
+                             />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                        <Typography variant="h5">{deaths.value}</Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                            start = {0}
+                            end = {deaths.value}
+                            duration = {2.5}
+                            separator = ","
+                             />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
                     </CardContent>
                 </Grid>
